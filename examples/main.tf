175ae8ad0d7b19e030dfb8d7a17fe5a8ed8b38f8
Gomex <rgomes@dnsfilter.com>
2024-10-24 14:12:00 -0300

    fix

--- examples/main.tf
+++ examples/main.tf
@@ -1,23 +1,6 @@
-# lookup AMI 
-data "aws_ami" "this" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["amazon-eks-node-1.27-v*"]
-  }
 
-  owners = "602401143452"
-}
-
-locals {
-  user_data = <<EOF
-#!/bin/bash
-sudo hostnamectl set-hostname test.test
-EOF
-}
-
-module "ec2" {
+module "EKS" {
   source = "../"
 
   team         = "platform"
